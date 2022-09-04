cab21f032d44bca115d086337e20fffb3853d7f3
DJV <djtvaq@gmail.com>
2022-09-04 18:36:00 -0400

    Add event listeners to trigger grid square hover styling

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let container = document.querySelector('.container')
-let newDiv = document.createElement('div')
+const container = document.querySelector('.container')
+const newDiv = document.createElement('div')
 
 newDiv.classList.add('gridSquare')
 
 for (let i = 0; i < 256; i++) {
     container.appendChild(newDiv.cloneNode(true))
 }
+
+function gridSquareMouseOver() {
+    this.classList.add('gridSquareHover')
+}
+
+function gridSquareMouseOut(){
+    this.classList.add('gridSquareReset')
+    this.classList.remove('gridSquareHover')
+}
+
+const gridSquare = document.querySelectorAll('.gridSquare')
+gridSquare.forEach(square => square.addEventListener('mouseover', gridSquareMouseOver))
+
+gridSquare.forEach(square => square.addEventListener('mouseout', gridSquareMouseOut))
+
+
+
