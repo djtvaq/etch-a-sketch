a9d3feb3abeb70bc595fc721a3352f3a8ff99d08
DJV <djtvaq@gmail.com>
2022-09-04 17:50:15 -0400

    Fix error, create 256 divs instead of 16

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let newDiv = document.createElement('div')
 
 newDiv.classList.add('gridSquare')
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     container.appendChild(newDiv.cloneNode(true))
 }
